263b05944e24f34cbe535755a56d8550503840bc
user-siva <sivag0503@gmail.com>
2022-03-31 20:15:46 +0530

    tweet ui created

--- client/twitter3.0/components/Home/Feed.jsx
+++ client/twitter3.0/components/Home/Feed.jsx
@@ -1,11 +1,38 @@
 import { BsStars } from 'react-icons/bs'
 import TweetBox from './TweetBox';
-
+import Post from "../Post"
 const style = {
     wrapper: `flex-[2] border-r border-l border-[#38444d]`,
     header: `sticky top-0 bg-[#15202b] z-10 p-4 flex justify-between items-center`,
     headerTitle: `text-xl font-bold`,
 }
+
+const tweets = [
+    {
+        displayname: "Siva",
+        username: '0x62f6B9D5d1c74c9952FFED81EC1537A07131982F',
+        avatar: 'https://wallpapercave.com/wp/wp1880364.jpg',
+        text: 'Hey!there',
+        isProfileImageNft: false,
+        timestamp: '2020-03-30'
+    },
+    {
+        displayname: "Siva",
+        username: '0x62f6B9D5d1c74c9952FFED81EC1537A07131982F',
+        avatar: 'https://wallpapercave.com/wp/wp1880364.jpg',
+        text: 'Hey!there',
+        isProfileImageNft: false,
+        timestamp: '2020-03-30'
+    },
+    {
+        displayname: "Siva",
+        username: '0x62f6B9D5d1c74c9952FFED81EC1537A07131982F',
+        avatar: 'https://wallpapercave.com/wp/wp1880364.jpg',
+        text: 'Hey!there',
+        isProfileImageNft: false,
+        timestamp: '2020-03-30'
+    }
+]
 
 function Feed() {
     return (
@@ -15,6 +42,17 @@ function Feed() {
                 <BsStars />
             </div>
             <TweetBox />
+            {tweets.map((tweet, index) => (
+                < Post
+                    key={index}
+                    displayName={tweet.displayname}
+                    username={`${tweet.username.slice(1, 4)}...${tweet.username - 4}`}
+                    avatar={tweet.avatar}
+                    text={tweet.text}
+                    isProfileImageNft={tweet.isProfileImageNft}
+                    timestamp={tweet.timestamp}
+                />
+            ))}
         </div>
     );
 }
